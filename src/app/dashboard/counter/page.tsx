b2590b689760ba48a9f4f4fee5b01ce2d9c90a59
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-28 22:27:12 +0200

    change name

--- src/app/dashboard/counter/page.tsx
+++ src/app/dashboard/counter/page.tsx
@@ -1,6 +1,6 @@
 import { Metadata } from 'next';
 
-import { CartCounter } from '@/shopping-card';
+import { CartCounter } from '@/shopping-cart';
 
 export const metadata: Metadata = {
   title: 'Shopping Cart',
