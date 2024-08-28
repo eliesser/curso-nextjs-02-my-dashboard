a0846042bd134adbf72747a6cb16db85fcd2f2d3
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-28 19:38:40 +0200

    add pokemons page component

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { IoBrowsersOutline, IoCalculator, IoLogoReact } from 'react-icons/io5';
+import { IoBrowsersOutline, IoCalculator, IoFootball, IoLogoReact } from 'react-icons/io5';
 
 import { SidebarMenuItem } from './SidebarMenuItem';
 
@@ -15,6 +15,12 @@ const menuItems = [
     icon: <IoCalculator size={40} />,
     title: 'Counter',
     subTitle: 'Counter Client Side',
+  },
+  {
+    path: '/dashboard/pokemons',
+    icon: <IoFootball size={40} />,
+    title: 'Pokemons',
+    subTitle: 'Generate static',
   },
 ];
 
---
+++ src/app/dashboard/pokemons/page.tsx
@@ -0,0 +1,7 @@
+export default function PokemonsPage() {
+  return (
+    <>
+      <h1>Pokemons Page</h1>
+    </>
+  );
+}
