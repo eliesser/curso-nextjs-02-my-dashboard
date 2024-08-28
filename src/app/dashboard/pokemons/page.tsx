885e2cb58050cea01dbaf116b16fc6acd970255c
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-28 21:02:11 +0200

    create pokemon grid component

--- src/app/dashboard/pokemons/page.tsx
+++ src/app/dashboard/pokemons/page.tsx
@@ -1,5 +1,4 @@
-import { PokemonsResponse, SimplePokemon } from '@/app/pokemons';
-import Image from 'next/image';
+import { PokemonGrid, PokemonsResponse, SimplePokemon } from '@/app/pokemons';
 
 const getPokemons = async (limit = 20, offset = 0): Promise<SimplePokemon[]> => {
   const pokemonsResponse: PokemonsResponse = await fetch(
@@ -13,6 +12,8 @@ const getPokemons = async (limit = 20, offset = 0): Promise<SimplePokemon[]> => 
     };
   });
 
+  throw new Error('This is a error');
+
   return pokemons;
 };
 
@@ -21,17 +22,11 @@ export default async function PokemonsPage() {
 
   return (
     <div className='flex flex-col'>
-      <div className='flex flex-wrap gap-10 items-center justify-center'>
-        {pokemons.map((pokemon) => (
-          <Image
-            key={pokemon.id}
-            src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${pokemon.id}.svg`}
-            width={100}
-            height={100}
-            alt={pokemon.name}
-          />
-        ))}
-      </div>
+      <span className='text-5xl my-2'>
+        List Pokemon <small>static</small>
+      </span>
+
+      <PokemonGrid pokemons={pokemons} />
     </div>
   );
 }
