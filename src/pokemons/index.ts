5906e961b76d1fd4176cf23a7cae3664c2320f36
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-28 22:19:01 +0200

    add interface pokemon

--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -3,3 +3,4 @@ export { PokemonCard } from './components/PokemonCard';
 
 export type { PokemonsResponse } from './interfaces/pokemons-response';
 export type { SimplePokemon } from './interfaces/simple-pokemon';
+export type { Pokemon } from './interfaces/pokemon';
