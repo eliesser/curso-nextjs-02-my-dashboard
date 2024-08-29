6ccfe581974b05d7a87d2bf938541a9a9a2d764e
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-29 12:25:40 +0200

    use store

--- src/shopping-cart/components/CartCounter.tsx
+++ src/shopping-cart/components/CartCounter.tsx
@@ -1,34 +1,35 @@
 'use client';
 
-import { useState } from 'react';
+import { decrement, increment, init, reset } from '@/store/counter/counterSlice';
+import { useAppSelector, useAppDispatch } from '@/store/store';
+import { useEffect } from 'react';
 
 interface Props {
   value?: number;
 }
 
 export const CartCounter = ({ value = 0 }: Props) => {
-  const [counter, setCounter] = useState(value);
+  const count = useAppSelector((state) => state.counter.count);
+  const dispatch = useAppDispatch();
 
-  const updateCounter = (count: number) => {
-    if (count < 0) count = 0;
-
-    setCounter(count);
-  };
+  useEffect(() => {
+    dispatch(init(value));
+  }, [dispatch, value]);
 
   return (
     <>
-      <span className='text-9xl'> {counter} </span>
+      <span className='text-9xl'> {count} </span>
 
       <div className='flex'>
         <button
           className='flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2'
-          onClick={() => updateCounter(counter - 1)}
+          onClick={() => dispatch(decrement())}
         >
           -1
         </button>
         <button
           className='flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2'
-          onClick={() => updateCounter(counter + 1)}
+          onClick={() => dispatch(increment())}
         >
           +1
         </button>
