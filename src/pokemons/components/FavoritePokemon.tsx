90f34cccf2310f8d02b860dc997b5b24ecba4027
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-29 13:42:11 +0200

    create NoFavorites components

--- src/pokemons/components/FavoritePokemon.tsx
+++ src/pokemons/components/FavoritePokemon.tsx
@@ -2,8 +2,22 @@
 
 import { useAppSelector } from '@/store/store';
 import { PokemonGrid } from './PokemonGrid';
+import { useState } from 'react';
+import { IoHeartOutline } from 'react-icons/io5';
 
 export const FavoritePokemon = () => {
   const pokemons = useAppSelector((state) => Object.values(state.pokemons));
-  return <PokemonGrid pokemons={pokemons} />;
+  const [favoritePokemons, setFavoritePokemons] = useState(pokemons);
+
+  return (
+    <>{favoritePokemons.length ? <PokemonGrid pokemons={favoritePokemons} /> : <NoFavorites />}</>
+  );
+};
+
+export const NoFavorites = () => {
+  return (
+    <div className='flex flex-col h-[50vh] items-center justify-center'>
+      <IoHeartOutline size={100} className='text-red-500' /> <span>No favorites</span>
+    </div>
+  );
 };
