7d1d42d19c283fc16ff042e3f33ee071380055a7
Eliesser Freites <freiteseliesser@hotmail.com>
2024-08-29 13:10:15 +0200

    create store for pokemons

---
+++ src/store/pokemons/pokemonsSlice.ts
@@ -0,0 +1,21 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+import { SimplePokemon } from '@/pokemons';
+
+interface PokemonsState {
+  [key: string]: SimplePokemon;
+}
+
+const initialState: PokemonsState = {
+  '1': { id: '1', name: 'bulbasaur' },
+};
+
+const pokemonsSlice = createSlice({
+  name: 'pokemons',
+  initialState,
+  reducers: {},
+});
+
+export const {} = pokemonsSlice.actions;
+
+export default pokemonsSlice.reducer;
